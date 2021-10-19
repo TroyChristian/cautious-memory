45175dcc29a36e580b2973aa588b4d2ffeac3aa8
TroyChristian <apostasy8@gmail.com>
2021-10-19 18:55:17 -0400

    Add Portfolio, Asset, and Entry models to models.py, Register their admin models. Add templates/CMApp, add CMApp to settings.py

--- CME/cautious_memory_project/CMApp/admin.py
+++ CME/cautious_memory_project/CMApp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from django.db import models
+from . import models as myModels
 # Register your models here.
+
+#class AuthorAdmin(admin.ModelAdmin):
+    #pass
+#admin.site.register(Author, AuthorAdmin)
+
+@admin.register(myModels.Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(myModels.Asset)
+class AssetAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(myModels.Entry)
+class EntryAdmin(admin.ModelAdmin):
+    pass
