ce1d4aea6a2153bd66d2b3c2afb3d829b99297ba
TroyChristian <apostasy8@gmail.com>
2021-10-29 16:41:57 -0400

    Add register form, add entry form

--- CME/cautious_memory_project/CMApp/views.py
+++ CME/cautious_memory_project/CMApp/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render, HttpResponse
+from . forms import EntryForm, CustomUserCreationForm
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
-    return render(request, "CMApp/index.html")
+    new_entry_form = EntryForm()
+    context= {"entry_form":new_entry_form}
+
+    return render(request, "CMApp/index.html", context=context)
+
+def register(request):
+    if request.method == 'POST':
+        f = CustomUserCreationForm(request.POST)
+        if f.is_valid():
+            f.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('register')
+
+    else:
+        f = CustomUserCreationForm()
+
+    return render(request, 'CMApp/register.html', {'form': f})
