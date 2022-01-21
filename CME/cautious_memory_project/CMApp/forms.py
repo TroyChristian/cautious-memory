21aeb7013584bb031add11ab2ca28f03bb1c5efb
Tyler Christian <tylerc71197@gmail.com>
2022-01-21 15:54:41 -0500

    added bootstrap and made changes to login/journal

--- CME/cautious_memory_project/CMApp/forms.py
+++ CME/cautious_memory_project/CMApp/forms.py
@@ -1,9 +1,10 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, fields
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
+from django.forms.widgets import TextInput
 from . models import Entry, Asset, Portfolio
 from decimal import Decimal
 
@@ -46,6 +47,7 @@ class CustomUserCreationForm(forms.Form):
             self.cleaned_data['password1']
         )
         return user
+
 
 
 class EntryForm(ModelForm):
